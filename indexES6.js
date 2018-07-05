7772244cfd09cacb85a05aede802d051c2393a1c
Skelethor <regis.garofalo@gmail.com>
2018-07-05 15:15:35 +0200

    Add files via upload

---
+++ indexES6.js
@@ -0,0 +1,7 @@
+let city = "Bidart" ;
+let age = 18;
+const dateofBirth = "26 janvier 1982";
+
+let sentence = `J'habite à ${city}, j'ai ${age} ans et je suis né le ${dateofBirth}`;
+
+console.log(sentence);
